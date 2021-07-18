9f553f21fffc7129e58fe676f93d0e39d50bd934
Cfffdy <p.rudenko44@gmail.com>
2021-07-18 15:23:05 +0300

    HW3

---
+++ hw3.py
@@ -0,0 +1,61 @@
+# №2
+a = "Hello World"
+s = 0
+for i in a:
+    print(i, end="")
+print()
+for j in a:
+    s += -1
+    print(a[s], end="")
+# №3
+b = 9
+
+for x in range(10):
+    print("*" * x)
+for z in range(b):
+    b -= 1
+    print("*" * b)
+# №4
+
+A = int(input("Number:"))
+B = int(input("Number:"))
+for x in range(A, B + 1):
+    print(x)
+# №5
+E = int(input("Number:"))
+F = int(input("Number:"))
+for s in range(E, F + 1):
+    print(s)
+print("End")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
